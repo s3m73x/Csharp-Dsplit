daa0bb305a9a5eb9d17e180677736cb27cbaa652
felixvossel <codes@fvossel.de>
2019-05-25 11:36:41 +0200

    updated comments
    Updatet comments

--- Dsplit/Program.cs
+++ Dsplit/Program.cs
@@ -1,18 +1,6 @@
-﻿/* This program is published under the  GNU GENERAL PUBLIC LICENSE!
- * Copyright (C) 1989, 1991 Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- * Everyone is permitted to copy and distribute verbatim copies
- * of this license document, but changing it is not allowed.
- * 
- * For more information search the GNU GENERAL PUBLIC LICENSE on the internet!
- */
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Text;
-using System.Threading.Tasks;
-using System.IO; // For reading and writing files
+using System.IO; 
 
 namespace Dsplit
 {
@@ -20,29 +8,72 @@ namespace Dsplit
     {
         static void Main(string[] args)
         {
-            if (args.Length!=4)                                             // throws a error output if the wrong number of argument given
-            {                                                               
-                Console.WriteLine("Es müssen 3 Parameter angeben werden");
-                                                                                    
+            ulong bytesize=new ulong(); //The size of the spitted files
+
+            ulong startpos = new ulong(); //The position to start splitting
+
+            int max = 0; //The position to end splitting
+
+            string filename = null; //The filename with complete path info
+
+            if (args.Length != 4) //If no arguments given, the user has to type them in                                          
+            {
+                Console.Write("Type in the prefered size of the splitted files:");
+
+                bytesize = Convert.ToUInt64(Console.ReadLine());
+
+                Console.Write("Type in the potion where to start with splitting:");
+
+                startpos = Convert.ToUInt64(Console.ReadLine());
+
+                Console.Write("Should the file be split up to the end?:");
+
+                string toend = Console.ReadLine().ToLower();
+
+                if (toend != "yes")
+                {
+                    Console.Write("Type in the position where to end with splitting:");
+
+                    max = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.Write("Type in the filename with full path:");
+
+                filename = Console.ReadLine();
+
             }
             else
             {
-                int max = 0;
-                ulong bytesize = Convert.ToUInt64(args[1]);                // size splitting packets
-                ulong startpos = Convert.ToUInt64(args[0]);                // position to start splitting
-                if (args[2].ToLower()!="end" && args[2].ToLower()!="max")  // Check that the user want to split until the end or not
-                {                                               
+
+
+                bytesize = Convert.ToUInt64(args[1]);
+
+                startpos = Convert.ToUInt64(args[0]);
+
+                if (args[2].ToLower() != "end" && args[2].ToLower() != "max") 
+                {
                     max = Convert.ToInt32(args[2]);
                 }
-                string filename = args[3];                                 // The path and name of the file that should be splitted
-                int p = filename.LastIndexOf(@"\");
-                string file = filename.Substring(p+1);
-                int p1 = file.LastIndexOf(".");
-                string end = file.Substring(p1);                           // Get the fileextension
+
+                filename = args[3];                                 
+            }
+
+                int p = filename.LastIndexOf(@"\"); //The positione of the last Backslash
+
+                string file = filename.Substring(p+1); //The filename without the path
+
+                int p1 = file.LastIndexOf("."); //The position of the last dot
+
+                string end = file.Substring(p1);   // Get the fileextension
+
                 file = file.Remove(p1);
+
+                Directory.SetCurrentDirectory(filename.Remove(filename.LastIndexOf(@"\")));
                
                 Directory.CreateDirectory(Directory.GetCurrentDirectory()+@"\splittedfiles"); // Create the Direcoty where the files will be saved
-                int a = 0;
+
+                int byteblockcounter = 0;
+
                 using (BinaryReader r = new BinaryReader(File.OpenRead(filename), Encoding.Unicode, true)) // Open the file to read
                 {
                     if (max != 0)                                                                          // Make shure that the end position is greater than 0
@@ -66,8 +97,8 @@ namespace Dsplit
                                 }
                                 if (r.BaseStream.Position >= Convert.ToInt32(startpos))
                                 {
-                                    a++;                                                               
-                                    using (FileStream f = new FileStream(Directory.GetCurrentDirectory() + @"\splittedfiles\" + file + a.ToString() + end, FileMode.Create, FileAccess.Write))
+                                    byteblockcounter++;                                                               
+                                    using (FileStream f = new FileStream(Directory.GetCurrentDirectory() + @"\splittedfiles\" + file + byteblockcounter.ToString() + end, FileMode.Create, FileAccess.Write))
                                     {
                                         f.Write(bytes, 0, Convert.ToInt32(bytesize)); // Write the buffer into a file
                                     }
@@ -89,8 +120,8 @@ namespace Dsplit
                                 }
                                 if (r.BaseStream.Position >= Convert.ToInt32(startpos))
                                 {
-                                    a++;
-                                    using (FileStream f = new FileStream(Directory.GetCurrentDirectory() + @"\splittedfiles\" + file + a.ToString() + end, FileMode.Create, FileAccess.Write))
+                                    byteblockcounter++;
+                                    using (FileStream f = new FileStream(Directory.GetCurrentDirectory() + @"\splittedfiles\" + file + byteblockcounter.ToString() + end, FileMode.Create, FileAccess.Write))
                                     {
                                         f.Write(bytes, 0, Convert.ToInt32(bytesize));
                                     }
@@ -121,8 +152,8 @@ namespace Dsplit
                                 }
                                 if (r.BaseStream.Position >= Convert.ToInt32(startpos))
                                 {
-                                    a++;
-                                    using (FileStream f = new FileStream(Directory.GetCurrentDirectory() + @"\splittedfiles\" + file + a.ToString() + end, FileMode.Create, FileAccess.Write))
+                                    byteblockcounter++;
+                                    using (FileStream f = new FileStream(Directory.GetCurrentDirectory() + @"\splittedfiles\" + file + byteblockcounter.ToString() + end, FileMode.Create, FileAccess.Write))
                                     {
                                         f.Write(bytes, 0, Convert.ToInt32(bytesize));
                                     }
@@ -144,8 +175,8 @@ namespace Dsplit
                                 }
                                 if (r.BaseStream.Position >= Convert.ToInt32(startpos))
                                 {
-                                    a++;
-                                    using (FileStream f = new FileStream(Directory.GetCurrentDirectory() + @"\splittedfiles\" + file + a.ToString() + end, FileMode.Create, FileAccess.Write))
+                                    byteblockcounter++;
+                                    using (FileStream f = new FileStream(Directory.GetCurrentDirectory() + @"\splittedfiles\" + file + byteblockcounter.ToString() + end, FileMode.Create, FileAccess.Write))
                                     {
                                         f.Write(bytes, 0, Convert.ToInt32(bytesize));
                                     }
@@ -160,4 +191,4 @@ namespace Dsplit
             
         }
     }
-}
+
